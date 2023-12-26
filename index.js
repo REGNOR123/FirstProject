7bde7fc58c1a27c877bdbc1ad5f2feef394eee25
Amit Kumar <amit1711030007@gmail.com>
2023-12-26 21:54:37 +0530

    use_of_gitignore_file

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const myServer = require('http') // importing 'http' modeule to create server
+// const myServer = require('http') // importing 'http' modeule to create server
+const colors = require('colors')
+// myServer.createServer((request,response)=> {              // 'createServer' alwayes recive two functions as a parameter
 
-myServer.createServer((request,response)=> {              // 'createServer' alwayes recive two functions as a parameter
+//     response.write("Hello World".red); // we are sending some response to the client.
+//     response.end();  // in order to end the response used end(), otherwise nodeJs keep responding.
 
-    response.write("<h1>Hello World</h1>"); // we are sending some response to the client.
-    response.end();  // in order to end the response used end(), otherwise nodeJs keep responding.
-
-}).listen('1999'); // using method 'createServer' and binding it with a port ,where it be will run
+// }).listen('1999'); // using method 'createServer' and binding it with a port ,where it be will run
+console.log("Hello World".red);
+console.log("Hello World".white);
+console.log("Hello World".green);
